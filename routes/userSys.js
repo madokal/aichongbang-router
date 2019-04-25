34c5ca0a426440b6080246ac33b32f0c9b463edc
tangfan <1294575646@qq.com>
2019-04-25 11:28:29 +0800

    上传路由

--- routes/userSys.js
+++ routes/userSys.js
@@ -1,10 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const client = require("ykt-http-client");
 client.url("localhost:8080");
 
-//用户管理
-router.post("/", async function(req, res) {
-    res.send("wsd")
+//获取全部店铺管理员数据
+router.get("/", async function(req, res) {
+  let { page, rows, type, value } = req.query;
+  let option = {};
+  console.log(123123)
+  if (type && value) {
+    option = { [type]: value };
+  }
+  let data = await client.get("/users",{"role":"店铺管理员",page, rows, ...option});
+    res.send(data);
 });
+
+//获取一条店铺管理员数据
+router.get("/:id", async function(req, res) {
+  let id = req.params.id;
+  let data = await client.get("/users/" + id);
+  res.send(data);
+});
+
+//删除店铺管理员
+router.delete("/:id", async function(req, res) {
+  let id = req.params.id;
+  let data = await client.delete("/users/" + id);
+  res.send(data);
+});
+
 module.exports = router;
